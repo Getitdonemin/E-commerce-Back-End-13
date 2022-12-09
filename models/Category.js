396fbcb561c18715213b5bbbc80238b7c201492e
Your Name <getitdonemin@gmail.com>
2022-12-09 02:20:21 -0600

    finished filling out category.js and Product.js

--- models/Category.js
+++ models/Category.js
@@ -2,12 +2,28 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    // id category
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Name of the category
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
   },
+
   {
     sequelize,
     timestamps: false,
